86ad9df8ca357e16c0796930eac47f3d10334e5f
McDumfly <schafer2507@gmail.com>
2024-12-14 10:12:21 +0100

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,7 +12,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
+  })
 
   it(`should have the 'todo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
